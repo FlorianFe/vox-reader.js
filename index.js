73fa50d37d87669404977367181ebd03dc2edb81
Florian Fechner <florian@Florians-MBP.fritz.box>
2018-06-06 15:21:16 +0200

    set up for npm

--- index.js
+++ index.js
@@ -1,14 +1,2 @@
 
-const fs = require('fs');
-const util = require('util')
-
-const readVoxFile = require('./src/readVox');
-
-fs.readFile('test.vox', (error, buffer) =>
-{
-  if (error) throw error;
-
-  const voxData = readVoxFile(buffer);
-
-  console.log(util.inspect(voxData, { showHidden: false, depth: null }))
-});
+module.exports = require('./src/readVox');
