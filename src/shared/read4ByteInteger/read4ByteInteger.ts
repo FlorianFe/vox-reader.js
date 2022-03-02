f7706a106d82840ee9ded835174cc2dd95b23b5a
Maximilian Gaedig <mg@maximiliangaedig.com>
2022-03-02 12:25:38 +0100

    🐛 Change uint to int

--- src/shared/read4ByteInteger/read4ByteInteger.ts
+++ src/shared/read4ByteInteger/read4ByteInteger.ts
@@ -1,5 +1,5 @@
 const read4ByteInteger = (data : Array<number>) : number =>
-  data.map((num, index) => (num * 2 ** (8 * index)) )
-      .reduce((sum, summand) => sum + summand, 0);
+  (data.map((num, index) => (num * 2 ** (8 * index)) )
+      .reduce((sum, summand) => sum + summand, 0) << 32) >> 32;
 
 export = read4ByteInteger;
